c406f520f6c99dad764d943d0d23a4b01f18e0a8
franklin8197 <franklin8197@gmail.com>
2022-10-18 15:21:56 +0530

    logout and navigaion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect, } from "react";
 // import Sidebar from "../components/Sidebar";
 const Home = () => {
+ const navigate = useNavigate();
+  useEffect(()=> {
+    navigate("/kovil/home-post")
+  },[])
   return (
     <>
       <Navbar />
